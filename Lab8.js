b7b3aba3d95c1dcf2b7dc78102e9836363db0018
GurkiratcheemaSecE <157052351+GurkiratcheemaSecE@users.noreply.github.com>
2024-03-16 17:18:22 -0400

    Add files via upload

---
+++ Lab8.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    // Filter images based on category
+    $(".btn").click(function(){
+    var category = $(this).data('filter');
+    if (category === 'all') {
+    $("#gallery > div").show();
+    } else {
+    $("#gallery > div").hide();
+    $("#gallery-" + category).show();
+    }
+    });
+    
+    // Lightbox functionality
+    $("#gallery").on("click", "img", function(){
+    var imgSrc = $(this).attr('src');
+    $("#lightbox-overlay").fadeIn();
+    $("#lightbox-content img").attr('src', imgSrc);
+    });
+    
+    $("#lightbox-close").click(function(){
+    $("#lightbox-overlay").fadeOut();
+    });
+    });
+    
